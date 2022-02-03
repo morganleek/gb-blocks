c2a5a986e7f2f8d51201e00ff7cd271f4288e275
Morgan Leek <morganleek@gmail.com>
2022-02-03 11:46:49 +0800

    versioning

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Extra blocks
  * Author: Morgan Leek
  * Author URI: https://morganleek.me/
- * Version: 1.0.16
+ * Version: 1.0.17
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
